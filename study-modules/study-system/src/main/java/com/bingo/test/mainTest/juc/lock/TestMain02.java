d2bc4edf20e0373a80cbc2ceb8053f7f6c3e033e
25865 <2586564239@qq.com>
2023-09-19 23:00:03 +0800

    study

---
+++ study-modules/study-system/src/main/java/com/bingo/test/mainTest/juc/lock/TestMain02.java
@@ -0,0 +1,33 @@
+package com.bingo.test.mainTest.juc.lock;
+
+/**
+ * javap -c xx.class
+ * <p>
+ * javap -v xx.class
+ *
+ * @author h-bingo
+ * @date 2023/09/17 15:44
+ **/
+public class TestMain02 {
+
+    Object object = new Object();
+
+    public void m1() {
+        synchronized (object) {
+            System.out.println("---------sync code block-----------");
+            throw new RuntimeException();
+        }
+    }
+
+    public synchronized void m2() {
+        System.out.println("---------sync method-----------");
+    }
+
+    public static synchronized void m3() {
+        System.out.println("---------sync static method-----------");
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
